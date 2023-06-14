2f689ee0f68222df8a754ac2c89f21dfe896a884
Emil Engfors <26778832+Engfors@users.noreply.github.com>
2023-06-14 07:43:14 +0200

    fixed outputs

--- terraform-base/outputs.tf
+++ terraform-base/outputs.tf
@@ -1,18 +1,18 @@
 output "vault" {
-  value = hcp_vault_cluster.vault
+  value     = hcp_vault_cluster.vault
   sensitive = true
 }
 
 output "hcp_vault_cluster_admin_token" {
-  value = hcp_vault_cluster_admin_token.token
+  value     = hcp_vault_cluster_admin_token.token
   sensitive = true
 }
 
 output "vault_url" {
-  value = data.hcp_vault_cluster.vault.public_endpoint ? (
-    data.hcp_vault_cluster.vault.vault_public_endpoint_url
+  value = hcp_vault_cluster.vault.public_endpoint ? (
+    hcp_vault_cluster.vault.vault_public_endpoint_url
     ) : (
-    data.hcp_vault_cluster.vault.vault_private_endpoint_url
+    hcp_vault_cluster.vault.vault_private_endpoint_url
   )
   description = "HCP Vault UI"
 }
